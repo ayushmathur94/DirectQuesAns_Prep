5688bc3e16a73f1c1a0383cc3647674ebb31b041
ayush <ayush@192.168.1.9>
2020-10-07 02:30:33 +0530

    firstcommit adp1

---
+++ src/adp_codingchallenge/FindSecondLargestNumberFrmArray.java
@@ -0,0 +1,9 @@
+package adp_codingchallenge;
+
+public class FindSecondLargestNumberFrmArray {
+
+	public static void main(String[] args) {
+		FindSecondLargestNumberFrmArray findOne = new FindSecondLargestNumberFrmArray();
+		System.out.println("I dont know what to do!!");
+	}
+}
